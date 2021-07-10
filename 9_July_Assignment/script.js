fecf8d4c8f6819279459a8d37caaedcd80ebbd87
Snehdeep-Kaur <86792691+Snehdeep-Kaur@users.noreply.github.com>
2021-07-10 17:31:54 +0530

    Delete 9_July_Assignment directory

--- 9_July_Assignment/script.js
+++
@@ -1,6 +0,0 @@
-function helloque()
-{
-   buttonv = document.querySelector('input').value;
-   button = document.querySelector('.mess');
-   button.innerText="Hello Everybody! My name is " + buttonv;
-}
