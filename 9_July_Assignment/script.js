fb41905a5320654ce6a8dff384c90f50c9745179
Snehdeep-Kaur <86792691+Snehdeep-Kaur@users.noreply.github.com>
2021-07-10 17:24:42 +0530

    JS code

---
+++ 9_July_Assignment/script.js
@@ -0,0 +1,6 @@
+function helloque()
+{
+   buttonv = document.querySelector('input').value;
+   button = document.querySelector('.mess');
+   button.innerText="Hello Everybody! My name is " + buttonv;
+}
